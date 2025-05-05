d67b89cdb43cedc64b64839aa008335df13eadc5
theDAOS <sanjusanthosh46@gmail.com>
2025-05-05 14:13:26 +0530

    theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { toggleTheme } from "../redux/bookSlice";
 
 function Navbar() {
-    return (
-        <div className="m-2 flex justify-between items-center bg-sky-200 p-3 rounded-md font-semibold border-2 shadow">
-            <Link to='/'>Library</Link>
+    let theme = useSelector((state) => state.bookSlice.theme)
+    let dispatch = useDispatch();
 
-            <button>Light Mode</button>
+    return (
+        <div className="m-2 p-2 flex justify-between items-center bg-sky-200 rounded-md font-semibold border-2 shadow">
+            <Link
+                to='/'
+            >Library</Link>
+
+            <button
+                className="bg-sky-950 px-2 py-1 rounded-md text-sky-50 shadow"
+                onClick={() => dispatch(toggleTheme())}
+            >
+                {theme} Mode
+            </button>
         </div>
     )
 }
