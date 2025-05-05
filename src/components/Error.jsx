9313f10616c9df106ea2a68debf35db14789c4eb
theDAOS <sanjusanthosh46@gmail.com>
2025-05-05 14:21:28 +0530

    error

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -6,7 +6,7 @@ function Error() {
     let dispatch = useDispatch();
 
     return (
-        <div className="m-2 mx-auto w-200 flex justify-around items-center gap-1 bg-red-400-200 p-1 rounded-md font-semibold border-2 shadow">
+        <div className="m-2 mx-auto w-200 flex justify-around items-center gap-1 bg-red-400 p-1 rounded-md font-semibold border-2 shadow">
             {error}
 
             <button
